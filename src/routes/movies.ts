d9bf85993e327e500085f0a02a60ac2a93814d42
Matheus Mendes <matheusmendes@Matheuss-MacBook.local>
2025-07-03 23:59:32 +0100

    update tsconfig and pack and req res

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import movies from '../data/movies.json';
 import { Movie } from '../types/Movie';
 
 const router = Router();
 const movieList: Movie[] = movies;
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   const { search, genre } = req.query;
 
   let results = movieList;
@@ -25,7 +25,7 @@ router.get('/', (req, res) => {
   res.json(results);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req: Request, res: Response) => {
   const movie = movieList.find(m => m.id === parseInt(req.params.id));
   if (!movie) return res.status(404).json({ error: 'Movie not found' });
   res.json(movie);
